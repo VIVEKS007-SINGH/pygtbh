e0f12d0a3b789c3984e3eea24506a3bde0056553
viveks007-singh <viveksoo067@gmail.com>
2023-08-23 01:02:37 +0530

    g

---
+++ python first program.py
@@ -0,0 +1,14 @@
+n= 5
+for i in range(n):
+    print(" "*(n-1) + "*" *i)
+    for i in range(n):
+     print(" "*(n-1) + "*" *i)
+
+for i in range(n-1,0,-1):
+    print(" "*(n-1) + "*" *i)
+
+x =10 
+for i in range (x):
+    print("hello"*i)
+    for j in range(x):
+        print ("vivek" *i)
